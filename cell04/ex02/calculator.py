857f1aa59b01722a69b9afcd8d6f0d8b13bcf37a
cgirakit <kahaii4424@gmail.com>
2026-09-23 20:09:54 +0700

    add ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+
+a = int(input("Give me the first number: "))
+b = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} / {b} = {a // b}")
+print(f"{a} * {b} = {a * b}")
